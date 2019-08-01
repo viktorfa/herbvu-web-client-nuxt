703237a00f117b905f2eb90c2ac526ab9ad87dc0
viktorfa <vikfand@gmail.com>
2019-08-01 18:32:06 +0200

    Remove console

--- src/pages/tilbud/_id.vue
+++ src/pages/tilbud/_id.vue
@@ -103,9 +103,6 @@ export default {
     // We don't use await on client, as that makes the page transition faster.
     // On SSR, we need the product to be fetched for SEO.
     if (process.client) {
-      console.log("is client");
-      console.log("params");
-      console.log(params);
       store.dispatch("LOAD_DETAIL_PRODUCT", {
         id: params.id,
       });

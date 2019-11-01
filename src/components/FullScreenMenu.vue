6fd0b05d34340dd2f5925d831058629d14ed301b
Viktor Andersen <vikfand@gmail.com>
2019-11-02 01:36:08 +0800

    Make home made full screen menu.

---
+++ src/components/FullScreenMenu.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <div>
+      <v-btn icon :aria-label="name" @click.stop="handleClickButton">
+        <v-icon class="text-t1">{{icon}}</v-icon>
+      </v-btn>
+    </div>
+    <div v-show="showMenu" class="w-full fixed left-0 top-0 bg-b1 h-screen z-10">
+      <div class="max-w-sm mx-auto py-2">
+        <div class="fixed right-0 h-12 flex align-center">
+          <button
+            class="p-2 text-2xl md:text-3xl"
+            @click="handleClickButton"
+            aria-label="Lukk meny"
+          >LUKK</button>
+        </div>
+        <div class="flex flex-col align-center">
+          <div>
+            <nuxt-link to="/" title="Hjem">
+              <img src="/logo-horizontal.svg" class="h-12 mx-auto" alt="Logo" />
+            </nuxt-link>
+          </div>
+          <div class="text-center text-2xl leading-loose">
+            <div>
+              <a
+                href="https://crowdsource.allematpriser.no"
+                target="_blank"
+                rel="noopener noreferrer"
+              >Legg til tilbud</a>
+            </div>
+            <div class="text-gray-800">
+              <span>Innstillinger</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FullScreenMenu",
+  computed: {
+    showMenu() {
+      return this.$route.hash.includes("menu=1");
+    },
+    icon() {
+      const { path } = this.$route;
+      return "menu";
+    },
+    name() {
+      const { path } = this.$route;
+      return "Åpne meny";
+    },
+  },
+  methods: {
+    handleClickButton() {
+      if (this.showMenu) {
+        this.$router.replace({ hash: "" });
+      } else {
+        this.$router.replace({ hash: "menu=1" });
+      }
+    },
+  },
+};
+</script>
+
+<style>
+</style>
--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,39 +1,38 @@
 <template>
-  <v-app>
-    <div>
-      <NavigationDrawer />
-      <div class="bg-b1 text-t1">
-        <div class="max-w-content mx-auto p-2">
-          <div class="flex flex-col md:flex-row">
-            <div class="h-12 my-1 text-center">
-              <nuxt-link to="/" name="Hjem">
-                <img src="/logo-horizontal.svg" class="h-full mx-auto" alt="Logo" />
-              </nuxt-link>
-            </div>
-            <div class="flex fluid my-auto justify-between md:ml-4 w-full">
-              <client-only placeholder>
-                <template v-slot:placeholder>
-                  <div class="search-bar-placeholder"></div>
-                </template>
-                <SearchBarComponent>
-                  <template v-slot:prepend-inner></template>
-                </SearchBarComponent>
-              </client-only>
-              <TopMenuButton />
-            </div>
+  <div>
+    <div class="bg-b1 text-t1">
+      <div class="max-w-content mx-auto p-1">
+        <div class="absolute right-0 h-12 my-1 flex items-center">
+          <FullScreenMenu />
+        </div>
+        <div class="flex flex-col md:flex-row">
+          <div class="h-12 my-1 text-center">
+            <nuxt-link to="/" name="Hjem">
+              <img src="/logo-horizontal.svg" class="h-full mx-auto" alt="Logo" />
+            </nuxt-link>
+          </div>
+          <div class="flex fluid my-auto justify-between md:ml-4 w-full">
+            <client-only placeholder>
+              <template v-slot:placeholder>
+                <div class="search-bar-placeholder"></div>
+              </template>
+              <SearchBarComponent>
+                <template v-slot:prepend-inner></template>
+              </SearchBarComponent>
+            </client-only>
           </div>
         </div>
       </div>
-      <div class="max-w-content mx-auto" id="main-content">
-        <nuxt />
-      </div>
-      <Footer />
     </div>
-  </v-app>
+    <div class="max-w-content mx-auto" id="main-content">
+      <nuxt />
+    </div>
+    <Footer />
+  </div>
 </template>
 
 <script>
-import TopMenuButton from "../components/TopMenuButton";
+import FullScreenMenu from "../components/FullScreenMenu";
 export default {
   components: {
     SearchBarComponent: () =>
@@ -42,11 +41,7 @@ export default {
       ),
     Footer: () =>
       import(/* webpackChunkName: "Footer" */ "../components/Footer.vue"),
-    NavigationDrawer: () =>
-      import(
-        /* webpackChunkName: "NavigationDrawer" */ "../components/NavigationDrawer.vue"
-      ),
-    TopMenuButton,
+    FullScreenMenu,
   },
 };
 </script>
